06978521ae78e69d4e6ab096cad2eebde6b8d315
Azat Temirbek <>
2023-02-12 22:43:14 +0300

    theme provided to styled components and react native paper

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import {MainRouter} from './router/MainRouter';
 import {Provider} from 'react-redux';
 import {store, persistor} from './config/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {theme} from './config/theme';
+import {ThemeProvider} from 'styled-components/native';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <MainRouter />
+        <ThemeProvider theme={theme}>
+          <PaperProvider theme={theme}>
+            <MainRouter />
+          </PaperProvider>
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   );
