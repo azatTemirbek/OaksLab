7ea59608d9e51770cfcf4c7136ccfea2f1d32aeb
Azat Temirbek <>
2023-02-12 23:01:28 +0300

    proper title added

--- src/screens/PhaseScreen.tsx
+++ src/screens/PhaseScreen.tsx
@@ -1,7 +1,23 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import {useMemo} from 'react';
 import {View, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {selectPhaseList} from '../redux/phase';
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '../router/MainRouter';
 
 export function PhaseScreen() {
+  const {setOptions} = useNavigation<StackNavigationProp>();
+
+  const data = useSelector(selectPhaseList);
+  console.log('data', data);
+
+  useEffect(() => {
+    setOptions({
+      title: 'My Startup Progress',
+    });
+  }, [setOptions]);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
