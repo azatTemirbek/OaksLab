bbb6daad7373f89f110e373dc4f603c36be12dbd
Azat Temirbek <>
2023-02-12 23:17:30 +0300

    memo added to section data and handlers added

--- src/screens/PhaseScreen.tsx
+++ src/screens/PhaseScreen.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react-native/no-inline-styles */
 import {useNavigation} from '@react-navigation/native';
-import React, {useEffect, useMemo} from 'react';
+import React, {useCallback, useEffect, useMemo} from 'react';
 import {SectionList} from 'react-native';
-import {useSelector} from 'react-redux';
-import {selectPhaseList} from '../redux/phase';
+import {useDispatch, useSelector} from 'react-redux';
+import {resetPhase, selectPhaseList, toggleTask} from '../redux/phase';
 import {StackNavigationProp} from '../router/MainRouter';
 import {Checkbox, Text, useTheme} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Entypo';
@@ -41,63 +42,100 @@ export const CheckIcon = styled(Icon).attrs({
 export function PhaseScreen() {
   const theme = useTheme();
   const {setOptions, navigate} = useNavigation<StackNavigationProp>();
-
   const data = useSelector(selectPhaseList);
+  const dispatch = useDispatch();
+
+  const sectionListData = useMemo(
+    () =>
+      data.map((phase, index) => ({
+        title: phase.name,
+        phaseIndex: index,
+        position: index + 1,
+        data: phase.tasks,
+      })),
+    [data],
+  );
+
+  const handleItemPress = useCallback(
+    ({phaseIndex, taskIndex}: {phaseIndex: number; taskIndex: number}) =>
+      () => {
+        dispatch(
+          toggleTask({
+            phaseIndex,
+            taskIndex,
+          }),
+        );
+      },
+    [dispatch],
+  );
+
+  const handleResetPress = useCallback(() => {
+    dispatch(resetPhase());
+  }, [dispatch]);
 
   useEffect(() => {
     setOptions({
+      headerRight: () => (
+        <Icon
+          name="reload-circle-outline"
+          size={24}
+          onPress={handleResetPress}
+        />
+      ),
       title: 'My Startup Progress',
     });
-  }, [setOptions]);
-
+  }, [handleResetPress, setOptions]);
   const isAllDataComplete = useMemo(
     () => data.every(phase => phase.tasks.every(task => task.isComplete)),
     [data],
   );
-
   useEffect(() => {
     if (isAllDataComplete) {
       navigate('RandomFact');
     }
-  }, [isAllDataComplete, navigate]);
+  }, [data, isAllDataComplete, navigate]);
 
   return (
-    <SectionList
-      sections={[
-        ...data.map((phase, phaseIndex) => ({
-          title: phase.name,
-          data: phase.tasks,
-          position: phaseIndex + 1,
-        })),
-      ]}
-      keyExtractor={(item, index) => `${item.id + index}`}
-      renderItem={({item}) => (
-        <ItemRow>
-          <Checkbox.Android
-            status={item.isComplete ? 'checked' : 'unchecked'}
-          />
-          <Text variant="titleMedium">{item.description}</Text>
-        </ItemRow>
-      )}
-      renderSectionHeader={({section: {title, data: tasks, position}}) => (
-        <SectionRow>
-          <BadgeView>
-            <Text
-              variant="headlineSmall"
-              style={{
-                color: theme.colors.onPrimary,
-                fontWeight: '700',
-              }}>
-              {position}
+    <>
+      <SectionList
+        sections={[...sectionListData]}
+        keyExtractor={(item, index) => `${item.id + index}`}
+        renderItem={({item, index, section: {phaseIndex}}) => (
+          <ItemRow
+            onPress={handleItemPress({
+              taskIndex: index,
+              phaseIndex,
+            })}>
+            <Checkbox.Android
+              status={item.isComplete ? 'checked' : 'unchecked'}
+              onPress={handleItemPress({
+                taskIndex: index,
+                phaseIndex,
+              })}
+            />
+            <Text variant="titleMedium">{item.description}</Text>
+          </ItemRow>
+        )}
+        renderSectionHeader={({section: {title, data: tasks, position}}) => (
+          <SectionRow>
+            <BadgeView>
+              <Text
+                variant="headlineSmall"
+                style={{
+                  color: theme.colors.onPrimary,
+                  fontWeight: '700',
+                }}>
+                {position}
+              </Text>
+            </BadgeView>
+            <Text variant="headlineSmall" style={{fontWeight: '700'}}>
+              {title}
             </Text>
-          </BadgeView>
-          <Text variant="headlineSmall" style={{fontWeight: '700'}}>
-            {title}
-          </Text>
-          {tasks.every(item => item.isComplete) && <CheckIcon />}
-        </SectionRow>
-      )}
-    />
+            {tasks.every(item => item.isComplete) && <CheckIcon />}
+          </SectionRow>
+        )}
+      />
+    </>
   );
 }
 
