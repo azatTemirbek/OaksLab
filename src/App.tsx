82e2a35a1f5fa1a94a93ee9cad04cb6cc84e0683
Azat Temirbek <>
2023-02-12 22:28:10 +0300

    configure redux store and persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import {MainRouter} from './router/MainRouter';
+import {Provider} from 'react-redux';
+import {store, persistor} from './config/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
-function App() {
-  return <MainRouter />;
+function App(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MainRouter />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/config/store.ts
@@ -0,0 +1,25 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+
+export const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: true,
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
