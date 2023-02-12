f538f3fedd9022fa4dcce09b0975d121493caa2e
Azat Temirbek <>
2023-02-12 22:56:08 +0300

    toggleTask action added and imports organized

--- src/redux/phase.ts
+++ src/redux/phase.ts
@@ -1,18 +1,55 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
-
+import {RootState} from '../config/store';
+import {initialData} from '../mockData/initialData';
+import {Phase} from '../Types/Phase';
 interface PhaseState {
-  phases: any[];
+  phases: Phase[];
 }
 
-const initialState = {phases: []} as PhaseState;
+const initialState = {phases: initialData} as PhaseState;
+
+const isPreviousPhaseComplete = (state: PhaseState, phaseIndex: number) => {
+  if (phaseIndex === 0) {
+    return true;
+  }
+  const previousPhase = state.phases[phaseIndex - 1];
+  return previousPhase.tasks.every(task => task.isComplete);
+};
+
+const isCurrentPhaseComplete = (state: PhaseState, phaseIndex: number) => {
+  const currentPhase = state.phases[phaseIndex];
+  return currentPhase.tasks.every(task => task.isComplete);
+};
 
 const phaseSlice = createSlice({
   name: 'phase',
   initialState,
-  reducers: {},
+  reducers: {
+    toggleTask(
+      state,
+      action: PayloadAction<{phaseIndex: number; taskIndex: number}>,
+    ) {
+      const {phaseIndex, taskIndex} = action.payload;
+      if (isCurrentPhaseComplete(state, phaseIndex)) {
+        state.phases[phaseIndex].tasks[taskIndex].isComplete =
+          !state.phases[phaseIndex].tasks[taskIndex].isComplete;
+        state.phases
+          .slice(phaseIndex + 1)
+          .forEach(phase =>
+            phase.tasks.forEach(task => (task.isComplete = false)),
+          );
+      } else if (isPreviousPhaseComplete(state, phaseIndex)) {
+        state.phases[phaseIndex].tasks[taskIndex].isComplete =
+          !state.phases[phaseIndex].tasks[taskIndex].isComplete;
+      }
+    },
+  },
 });
+
+export const selectPhaseList = (state: RootState) => state.phase.phases;
+export const {toggleTask} = phaseSlice.actions;
 
 const phasePersistConfig = {
   key: 'phase',
