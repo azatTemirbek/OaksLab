d349f36558f472ddcaa9d85a92fca113df0663df
Azat Temirbek <>
2023-02-12 23:05:00 +0300

    is all compleate listener added

--- src/screens/PhaseScreen.tsx
+++ src/screens/PhaseScreen.tsx
@@ -1,22 +1,32 @@
 import React, {useEffect} from 'react';
 import {useMemo} from 'react';
 import {View, Text} from 'react-native';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {selectPhaseList} from '../redux/phase';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '../router/MainRouter';
 
 export function PhaseScreen() {
-  const {setOptions} = useNavigation<StackNavigationProp>();
+  const {setOptions, navigate} = useNavigation<StackNavigationProp>();
 
   const data = useSelector(selectPhaseList);
-  console.log('data', data);
 
   useEffect(() => {
     setOptions({
       title: 'My Startup Progress',
     });
   }, [setOptions]);
+
+  const isAllDataComplete = useMemo(
+    () => data.every(phase => phase.tasks.every(task => task.isComplete)),
+    [data],
+  );
+
+  useEffect(() => {
+    if (isAllDataComplete) {
+      navigate('RandomFact');
+    }
+  }, [data, isAllDataComplete, navigate]);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
